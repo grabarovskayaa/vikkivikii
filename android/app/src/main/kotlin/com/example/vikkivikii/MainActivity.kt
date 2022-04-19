75d91d0ee115f2b4148f65f5b1425b27bd587fef
NastyyyaSham <samilskaya.a@edu.narfu.com>
2022-04-19 21:28:41 +0300

    KR

---
+++ android/app/src/main/kotlin/com/example/vikkivikii/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vikkivikii
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/recipe_detail.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+
+  const RecipeDetail({Key? key, required this.recipe}) : super(key: key);
+
+  @override
+  _RecipeDetailState createState() {
+    return _RecipeDetailState();
+  }
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label),
+      ),
+      // 2
+      body: SafeArea(
+        // 3
+        child: Column(
+          children: <Widget>[
+            // 4
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image(image:
+              AssetImage(widget.recipe.imageUrl)),
+            ),
+            // 5
+            const SizedBox(
+              height: 4,
+            ),
+            // 6
+            Text(
+              widget.recipe.label,
+              style: const TextStyle(fontSize: 30),
+            ),
+            // 7
+            Expanded(
+              // 8
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  // 9
+                  return Text(
+                      '${ingredient.measure} '
+                      '${ingredient.name}',
+                      style: TextStyle(fontSize: 18));
+                },
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
